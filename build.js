5d2abe23d69bb2662338a90b613e7435c52b6126
Michael Louis Thaler <michael.louis.thaler@gmail.com>
2026-04-19 18:57:32 -0400

    Node miscellanea

--- build.js
+++ build.js
@@ -5,22 +5,20 @@ import sveltePlugin from "esbuild-svelte";
 import { sveltePreprocess } from "svelte-preprocess";
 import { compile as sassCompile } from "sass";
 
-esbuild
-  .build({
-    entryPoints: ["modules/js/Game.ts"],
-    mainFields: ["svelte", "browser", "module", "main"],
-    conditions: ["svelte", "browser"],
-    bundle: true,
-    format: "esm",
-    outfile: "modules/js/Game.js",
-    plugins: [
-      sveltePlugin({
-        preprocess: sveltePreprocess(),
-      }),
-    ],
-    logLevel: "info",
-  })
-  .catch(() => process.exit(1));
+await esbuild.build({
+  entryPoints: ["modules/js/Game.ts"],
+  mainFields: ["svelte", "browser", "module", "main"],
+  conditions: ["svelte", "browser"],
+  bundle: true,
+  format: "esm",
+  outfile: "modules/js/Game.js",
+  plugins: [
+    sveltePlugin({
+      preprocess: sveltePreprocess(),
+    }),
+  ],
+  logLevel: "info",
+});
 
 const out = sassCompile("modules/css/theanarchy.scss", {
   style: "compressed",
