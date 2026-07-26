028afc56538d186c2a43f507f58989d0b0abb9cf
Michael Louis Thaler <michael.louis.thaler@gmail.com>
2026-07-26 16:49:49 -0400

    Add a simple Tooltip component

---
+++ modules/js/svelte/Tooltip.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+  import { getBga } from "../context.svelte";
+  import type { Attachment } from "svelte/attachments";
+
+  interface Props {
+    children: Snippet;
+  }
+  const { children }: Props = $props();
+  const id = $props.id();
+  const bga = getBga();
+
+  const tooltip: Attachment = (e) => {
+    bga.gameui.addTooltip(e.id, _("HELLO"), _("GOODBYE"));
+    return () => bga.gameui.removeTooltip(e.id);
+  };
+</script>
+
+<div {id} {@attach tooltip}>
+  {@render children()}
+</div>
